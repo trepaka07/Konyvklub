eb4d4ab20e902e36dbfe1a4efa0f01940d8e302b
Trepák Attila <bboytrepy@gmail.com>
2023-05-11 11:55:02 +0200

    demo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Könyvklub</title>
+    <title>Könyvklub - Demo</title>
     <link rel="shortcut icon" href="./img/icons/logo_notext.png" type="image/x-icon">
     <link rel="stylesheet" href="frontend/style/header.css">
     <link rel="stylesheet" href="./frontend/style/style.css">
